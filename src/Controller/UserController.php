7b8d4e9d3eb0e4d13a2e23dbb1b857183633c25a
Xora123 <kelian2009@hotmail.fr>
2022-01-13 16:50:03 +0100

    ajout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,19 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserFormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserController extends AbstractController
 {
-    #[Route('/inscription', name: 'register')]
-    public function registration(): Response
+    public function __construct(private ManagerRegistry $doctrine) {}
+    #[Route('/inscription', name: 'register', methods:["GET","POST"] )]
+    public function registration(Request $request): Response
     {
+        
         $user = new User();
-        $form = $this->createForm(user/form);
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $form = $this->createForm(UserFormType::class,$user)    
+        ->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            # Notification de confirmation
+            $this->addFlash("success", "Votre inscription a été validée");
+            # Redirection
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('user/registration.html.twig', [
+            "registrationForm"=>$form->createView()
+        ]); 
+        
     }
 }
--- src/Form/userformtype.php
+++ src/Form/userformtype.php
@@ -2,25 +2,27 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class , [
+            ->add('firstname', TextType::class , [
                 'label' => false,
                 'attr'=>[
                     "placeholder"=> 'Saisissez votre prénom...'
                 ],
             ])
-            ->add('lastName', TextType::class , [
+            ->add('lastname', TextType::class , [
                 'label' => false,
                 'attr'=>[
                     "placeholder"=> 'Saisissez votre nom...'
@@ -32,23 +34,11 @@ class UserFormType extends AbstractType
                     "placeholder"=> 'Saisissez votre email...'
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('password', TextType::class , [
                 'label' => false,
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password', "placeholder"=> 'Saisissez votre mot de passe...'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit comporter {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                'attr'=>[
+                    "placeholder"=> 'Saisissez votre mot de passe...'
+                ]
             ])
         ;
     }
