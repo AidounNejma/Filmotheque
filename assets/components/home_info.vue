e0723a344caa7eb0877f636f6147daccdf6a957c
AidounNejma <guermachenejma@gmail.com>
2022-01-16 13:45:03 +0100

    add css
    Ajout de style, du front sur la page d'accueil

---
+++ assets/components/home_info.vue
@@ -0,0 +1,38 @@
+    <template>
+            <div class="d-flex justify-content-between">
+                <div class="flex-md-column p-4 m-4 text-center">
+                    <img class="pb-4" src="/img/home/info1.png">
+                    <p>Découvrez le Service de <strong>Vidéo à la demande</strong> de Filmothèque !</p>
+                    <a href="" class="linksInfo">Voir les fonctionnalités</a>
+                </div>
+                <div class="flex-md-column p-4 m-4 text-center">
+                    <img class="pb-4" src="/img/home/info2.png">
+                    <p>Découvrez le Service de <strong>Vidéo à la demande</strong> de Filmothèque !</p>
+                    <a href="" class="linksInfo">Voir la liste</a>
+                </div>
+                <div class="flex-md-column p-4 m-4 text-center">
+                    <img class="pb-4" src="/img/home/info3.png">
+                    <p>Découvrez le Service de <strong>Vidéo à la demande</strong> de Filmothèque !</p>
+                    <a href="" class="linksInfo">Installer l'application</a>
+                </div>
+            </div>
+    </template>
+
+<script>
+
+    import Vue from 'vue'
+
+    export default {
+        components: {
+            info: Vue
+        }
+    }
+
+    const Info = new Vue({
+        el: '#info',
+        components: {
+            info: Vue 
+        } 
+    });
+
+</script>
--- assets/home.js
+++ assets/home.js
@@ -2,6 +2,8 @@ import './styles/home.css';
 
 //Imports Vue
 import Vue from 'vue'
-import Carousel from './components/carousel.vue'
+import Carousel from './components/home_carousel.vue'
+import Info from './components/home_info.vue'
 
 new Vue({ render: h => h(Carousel) }).$mount('#carousel')
+new Vue({ render: h => h(Info) }).$mount('#info')
