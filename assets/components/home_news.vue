4f92ec94f6710713e8dc41dcf327dd326db7295e
AidounNejma <guermachenejma@gmail.com>
2022-01-16 13:56:44 +0100

    add vue for "nouveautés" in"accueil"

---
+++ assets/components/home_news.vue
@@ -0,0 +1,39 @@
+    <template>
+        <div class="news p-4">
+            <h3 class="text-center text-white pb-4"> Nouveautés</h3>
+            <div class="d-flex">
+                <div class="firstDiv mx-4">
+                    <a href="" class="p-2 imgOne"><img alt="" src="/img/home/pulpfiction.png"></a>
+                </div>
+                <div class="d-flex flex-wrap px-4 px-4 secondPart">
+                    <a href="" class="p-2 imgEight"><img alt="" src="/img/home/avatar.png"></a>
+                    <a href="" class="p-2 imgEight"><img alt="" src="/img/home/inception.png"></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/joker.png" alt=""/></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/le_discours_dun_roi_affiche.png" alt=""/></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/toystory.png" alt=""/></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/thepaintedveil.png" alt="" /></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/matrix.png" alt=""/></a>
+                    <a href="" class="p-2 imgEight"><img src="/img/home/Le_Temoin_du_mal.png" alt="" /></a>
+                </div>
+            </div>
+        </div>
+    </template>
+
+<script>
+
+    import Vue from 'vue'
+
+    export default {
+        components: {
+            news: Vue
+        }
+    }
+
+    const News = new Vue({
+        el: '#news',
+        components: {
+            news: Vue 
+        } 
+    });
+
+</script>
--- assets/home.js
+++ assets/home.js
@@ -4,6 +4,8 @@ import './styles/home.css';
 import Vue from 'vue'
 import Carousel from './components/home_carousel.vue'
 import Info from './components/home_info.vue'
+import News from './components/home_news.vue'
 
 new Vue({ render: h => h(Carousel) }).$mount('#carousel')
 new Vue({ render: h => h(Info) }).$mount('#info')
+new Vue({ render: h => h(News) }).$mount('#news')
