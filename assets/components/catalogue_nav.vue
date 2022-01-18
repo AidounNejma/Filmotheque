251f4bce8440b09f76f1121dee26fdd39fd1b75e
AidounNejma <guermachenejma@gmail.com>
2022-01-18 17:38:37 +0100

    change link to button

--- assets/components/catalogue_nav.vue
+++ assets/components/catalogue_nav.vue
@@ -2,9 +2,9 @@
     <section class="d-flex flex-column">
         <h3 class="text-center pt-2"> Le Catalogue</h3>
         <div class="d-flex justify-content-center align-items-center">
-            <a href="#!" class="px-4 pt-2 pb-4 linksCatalogue">Programmes (1676)</a>
-            <a href="#!" class="px-4 pt-2 pb-4 linksCatalogue">Les réalisateurs</a>
-            <a href="#!" class="px-4 pt-2 pb-4 linksCatalogue">Les acteurs</a>
+            <button href="#" class="px-4 pt-2 pb-4 linksCatalogue">Programmes (1676)</button>
+            <button href="#" class="px-4 pt-2 pb-4 linksCatalogue">Les réalisateurs</button>
+            <button href="#" class="px-4 pt-2 pb-4 linksCatalogue">Les acteurs</button>
         </div>
     </section>
 </template>
